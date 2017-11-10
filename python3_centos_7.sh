70e535b18cf6049f7f5c95172bf8a323ece1ee4b
root <root@alienware.c.lithe-sonar-185507.internal>
2017-11-10 05:47:18 +0000

    Added script to install Python3

---
+++ python3_centos_7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script to install Python3 on CentOS 7
+
+# Preparing the System
+yum -y install yum-utils
+yum -y groupinstall development
+
+# Installing and Setting Up Python 3
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yum -y install python36u
+
+# Check Python version
+python3.6 -V
+
+# Install pip
+yum -y install python36u-pip
