32aab5d065c14b5e86cd476b9927a59de8d76f83
saurabhlambe <saurabh.lambe10@gmail.com>
2021-05-03 14:35:57 +0530

    Added a script to install docker on CentOS

---
+++ install_docker_centos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo # blank line
+echo "This script installs Docker on CentOS"
+echo # blank line
+
+echo "Remove previous docker versions if they exist"
+sudo yum remove docker                   docker-client                   docker-client-latest                   docker-common                   docker-latest                   docker-latest-logrotate                   docker-logrotate                   docker-engine
+echo # blank line
+
+echo "Set up Docker repo"
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+echo # blank line
+
+echo "Install Docker"
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+echo # blank line
+
+echo "Start and enable Docker"
+sudo systemctl start docker
+sudo systemctl enable docker
+echo # blank line
+
+echo "Verify the installation"
+sudo docker run hello-world
+echo # blank line
+
+echo "Verify docker version"
+docker --version
+
