2f36aecda4291b9ea992180c263ca016987c13fa
Saurabh Lambe <saurabh.lambe10@gmail.com>
2025-11-18 10:50:13 +0530

    made some edits to add env variables instead of client ID and secret

--- fetchArtistGenre.py
+++ fetchArtistGenre.py
@@ -1,6 +1,13 @@
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='088effd1d43c44c1b859616babe82da6', client_secret='dab4db21446140489cbc75ab7ec7ed58'))
-artist = sp.artist('spotify:artist:0ug84nvWi4PxvGIL52EZWr')
+# Set environment variables SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET before running the script
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
+    client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET')
+))
+
+# Set environment variable ARTIST_ID to the artist ID from Spotify
+artist = sp.artist('spotify:artist:ARTIST_ID')
 print(artist['name'], artist['genres'])
