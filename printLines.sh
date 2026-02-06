f9f9cef0db1686c6f062317a9d58d78f16d6abc0
Saurabh Lambe <saurabh.lambe10@gmail.com>
2026-02-06 12:47:47 +0530

    Rename variable userInput to userName

--- printLines.sh
+++ printLines.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This file just prints some lines on to the console
-read -p "Please enter your name: " userInput
+read -p "Please enter your name: " userName
 echo
-echo "Welcome, $userInput"
+echo "Welcome, $userName"
 
 
